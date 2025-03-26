a2e9e5b83480ff441976c5768d2d24d1dcb8acb9
Minh Tam Nguyen <tamnguyen277353@gmail.com>
2025-03-26 14:52:18 -0600

    Add project files.

---
+++ Testing/Testing.cs
@@ -0,0 +1,130 @@
+﻿using UsingSingleLinkedListLab;
+
+namespace Testing
+{
+    [TestFixture]
+    public class Testing
+    {
+        [Test]
+        public void AddNodesToTheBegining()
+        {
+            SingleLinkedList<string> people = new SingleLinkedList<string>();
+
+            double expectedCount = people.Count + 3;
+
+            people.AddFirst("John Smith");
+            people.AddFirst("Joe Schmoe");
+            people.AddFirst("Joe Blow");
+
+            double actualCount = people.Count;
+            string firstPerson = people.GetValue(0);
+
+            Assert.That(actualCount, Is.EqualTo(expectedCount));
+            Assert.That(firstPerson, Is.EqualTo("Joe Blow"));
+        }
+
+        [Test]
+        public void AddNodesToTheEnd()
+        {
+            SingleLinkedList<string> people = new SingleLinkedList<string>();
+
+            double expectedCount = people.Count + 3;
+
+            people.AddLast("Bob Bobberson");
+            people.AddLast("Sam Sammerson");
+            people.AddLast("Dave Daverson");
+
+            double actualCount = people.Count;
+            string lastPerson = people.GetValue(people.Count - 1);
+
+            Assert.That(actualCount, Is.EqualTo(expectedCount));
+            Assert.That(lastPerson, Is.EqualTo("Dave Daverson"));
+        }
+
+        [Test]
+        public void RemovingFirstNode()
+        {
+            SingleLinkedList<string> people = new SingleLinkedList<string>(); 
+
+            people.AddFirst("John Smith");
+            people.AddFirst("Joe Schmoe");
+            people.AddFirst("Joe Blow");
+
+            double expectedCount = people.Count - 1;
+
+            people.RemoveFirst();
+
+            //After remove "Joe Blow"
+            double actualCount = people.Count;
+            string firstPerson = people.GetValue(0);
+
+            Assert.That(actualCount, Is.EqualTo(expectedCount));
+            Assert.That(firstPerson, Is.EqualTo("Joe Schmoe"));
+        }
+
+        [Test]
+        public void RemovingLastNode()
+        {
+            SingleLinkedList<string> people = new SingleLinkedList<string>();
+
+            people.AddLast("Bob Bobberson");
+            people.AddLast("Sam Sammerson");
+            people.AddLast("Dave Daverson");
+
+            double expectedCount = people.Count - 1;
+
+            people.RemoveLast();
+
+            //After remove "Dave Daverson"
+            double actualCount = people.Count;
+            string lastPerson = people.GetValue(people.Count - 1);
+
+            Assert.That(actualCount, Is.EqualTo(expectedCount));
+            Assert.That(lastPerson, Is.EqualTo("Sam Sammerson"));
+        }
+
+        [Test]
+        public void RetrivingValueOfANode()
+        {
+            SingleLinkedList<string> people = new SingleLinkedList<string>();
+
+            people.AddFirst("John Smith");
+            people.AddFirst("Joe Schmoe");
+            people.AddFirst("Joe Blow");
+
+            string expected = "Joe Schmoe";
+            string actual = people.GetValue(1); //get "Joe Schmoe"
+
+            Assert.That(actual, Is.EqualTo(expected));
+        }
+
+        [Test]
+        public void DetermineTheSizeOfTheList() 
+        {
+            SingleLinkedList<string> people = new SingleLinkedList<string>();
+
+            people.AddFirst("John Smith");
+            people.AddFirst("Joe Schmoe");
+            people.AddFirst("Joe Blow");
+            people.AddLast("Jane Doe");
+            people.AddLast("Bob Bobberson");
+            people.AddLast("Sam Sammerson");
+            people.AddLast("Dave Daverson");
+
+            int expectedCount = 7;
+            int actualCount = people.Count;
+
+            Assert.That(actualCount, Is.EqualTo(expectedCount));
+        }
+
+        [Test]
+        public void RetrivingAndRemovingFromEmptyList()
+        {
+            SingleLinkedList<string> emptyList = new SingleLinkedList<string>();
+
+            Assert.Throws<Exception>(() => emptyList.GetValue(0));
+            Assert.Throws<Exception>(() => emptyList.RemoveFirst());
+            Assert.Throws<Exception>(() => emptyList.RemoveLast());
+        }
+    }
+}
---
+++ UsingSingleLinkedListLab/LinkedList.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UsingSingleLinkedListLab
+{
+    public class SingleLinkedList<T>
+    {
+        public Node<T>? Head { get; private set; }
+
+        public int Count { get; private set; }
+
+        public SingleLinkedList() 
+        {
+            Head = null;
+            Count = 0;
+        }
+
+        //methods
+        public void AddFirst(string value)
+        {
+            T convertedValue = (T)Convert.ChangeType(value, typeof(T));
+            Node<T> firstNode = new Node<T>(convertedValue);
+            if (Head != null)
+            {
+                firstNode.Next = Head;
+            }
+            Head = firstNode;
+            Count++;  
+        }
+
+        public void AddLast(string value)
+        {
+            T convertedValue = (T)Convert.ChangeType(value, typeof(T));
+            Node<T> newNode = new Node<T>(convertedValue);
+            Node<T>? current = Head;
+            if (Head == null)
+            {
+                Head = newNode;
+            }
+            else
+            {
+                while (current != null)
+                {
+                    if (current.Next  ==  null)
+                    {
+                        current!.Next = newNode;
+                        break;
+                    }
+                    current = current.Next;
+                }
+            }
+            Count++;
+        }
+        
+        public void RemoveFirst()
+        {
+            if (Head == null)
+            {
+                throw new Exception("List is empty.");
+            }
+            else
+            {
+                Head = Head.Next;
+                Count--;
+            }
+        }
+
+        public void RemoveLast()
+        {
+            Node<T>? current = Head;
+            if (current == null)
+            {
+                throw new Exception("List is empty.");
+            }
+            else
+            {
+                {
+                    while (current != null)
+                    {
+                        if (current.Next!.Next == null)
+                        {
+                            current.Next = null!;
+                            break;
+                        }
+                        current = current.Next;
+                    }
+                    Count--;
+                }
+            }
+        }
+
+        public string GetValue(int index)
+        {
+            Node<T>? current = Head;
+            if (current == null)
+            {
+                throw new Exception("List is empty.");
+            }
+            else
+            {
+                for(int i = 0 ; i <= index; i++)
+                {
+                    if (i == index)
+                    {
+                        return current.Value!.ToString()!;
+                    }
+                    current = current.Next;
+                }
+            }
+            throw new Exception("Index out of bounds.");
+        }
+    }
+}
